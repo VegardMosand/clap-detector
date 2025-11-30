70055756da84f9bec4eb7cc0211593865b28175d
VegardMosand <vegsnd@gmail.com>
2025-11-30 22:28:29 +0100

    Add live data readout

--- src/clap_detector.py
+++ src/clap_detector.py
@@ -2,11 +2,13 @@ import time
 import pathlib  
 import threading
 import os
-import librosa
+import alsaaudio
 import sys
 print("Importing torch. This may take a while...")
 import torch
 import numpy as np
+import librosa
+#import soundfile as sf
 import subprocess
 from torch import nn
 from clap_nn import ClapDetectNN
@@ -15,7 +17,7 @@ from clap_utils import create_2d_numpy
 import wave
 
 sample_rate = 16000
-sensitivity = 0.05 #0.05
+sensitivity = 0.55 #0.05
 
 livefile = "wearedoingitlive.wav"
 filename = "2claps_with_noise.wav"
@@ -28,9 +30,11 @@ def record():
 
 # generates chunks of the correct size and format
 def chunk_generator():
+    # Hop length of 100 ms. Each segment/chunk is 200 ms
     stream = librosa.stream(path=sys.argv[1], block_length=1, frame_length=3200, hop_length=1600, mono=True, dtype=np.float32)
     build_chunk : np.ndarray = np.array([])
     for chunk in stream:
+        # Handle bad chunk size
         if (chunk.size != 3200):
             remaining_chunk_size = 3200 - build_chunk.size
             if (remaining_chunk_size <= chunk.size):
@@ -45,7 +49,21 @@ def chunk_generator():
                 continue
         yield chunk
 
-# Stykk opp i filer. librosa load hver fil?
+def chunk_generator2():
+    new_chunk : np.ndarray
+    build_chunk : np.ndarray
+    # Each period will be half a chunk. This is to be able to make the overlap
+    inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device='plughw:1,0', rate=sample_rate, periodsize=1600, channels=1, format=alsaaudio.PCM_FORMAT_FLOAT_LE)
+    length, data = inp.read()
+    build_chunk = np.frombuffer(data, dtype=np.float32)
+    while True:
+        length, data = inp.read()
+
+        if length > 0:
+            new_chunk = np.frombuffer(data, dtype=np.float32)
+
+            yield np.append(build_chunk, new_chunk)
+            build_chunk = new_chunk
 
 def clap_listener():
     model = ClapDetectNN()
@@ -57,22 +75,26 @@ def clap_listener():
     # start (at least almost) of a frame. To avoid a clap being counted twice the first chunk after a clap detection is ignored
 
     #stream = librosa.stream(path=livefile, block_length=1, frame_length=3200, hop_length=1600, mono=True, dtype=np.float32)
-    losslist = []
-    stream =  chunk_generator()
+    #losslist = []
+    stream =  chunk_generator2()
     reslist = [1.0]
     reslist = np.array(reslist)
     reslist : torch.tensor = torch.from_numpy(reslist)
     claps = 0
+    i = 0
 
     prevclap = False
 
     with torch.no_grad():
         for chunk in stream:
             if(prevclap):
-                print("Previous chunk was a clap. Ignoring this one. (You cant clap that fast)")
                 prevclap =False
                 continue
 
+            if (i % 1000 == 0):
+                print(f"Got frame number {i}")
+
+            i += 1
             soundlist = []
 
             power_spectrogram = create_PS(chunk)
@@ -89,14 +111,15 @@ def clap_listener():
 
             if (loss < sensitivity):
                 claps += 1
-                losslist.append(loss)
-                print(f"Detected clap!! This was clap number {claps}. Loss: {loss}")
+                #losslist.append(loss)
+                print(f"Detected clap at {i*100}ms!! This was clap number {claps}. Loss: {loss}")
+                #sf.write(f"../candidates/candidate_{i}.wav", chunk, 16000)
                 prevclap = True
             else:
-                print(f"not a clap!! Loss {loss}")
                 prevclap = False
+                #print(f"not a clap!! Loss {loss}")
     
-    print(f"Done. Total number of claps: {claps}. losslist:\n{losslist}")
+    print(f"Done. Total number of claps: {claps}.") #losslist:\n{losslist}")
 
 
 #t1 = threading.Thread(target=record, args=[])
